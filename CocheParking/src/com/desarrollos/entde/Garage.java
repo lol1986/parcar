ff15b96bc9f32c861d7c47a98c52a3dc435e348c
Laura Ortiz <lauraortizluque@gmail.com>
2018-11-23 00:30:28 +0100

    Fix

--- CocheParking/src/com/desarrollos/entde/Garage.java
+++ CocheParking/src/com/desarrollos/entde/Garage.java
@@ -1,9 +1,11 @@
 package com.desarrollos.entde;
 
+
+
 public class Garage{
     
     public void printGarage() 
     {
-        system.out.print("|*]");
+        System.out.print("|*]");
     }
 }
